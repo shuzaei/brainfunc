f6d9bb71ac9155729db85c98d7a19219d18c4e9c
Shuz* <42332130+shuzaei@users.noreply.github.com>
2021-06-27 19:35:13 +0900

    Update install.sh

--- bcc/install.sh
+++ bcc/install.sh
@@ -3,8 +3,11 @@
 cd $(dirname $0)
 
 chmod +x bcc.sh
+if [ ! -d /usr/local/bin/ ]; then
+    sudo mkdir -p /usr/local/bin/
+fi
 sudo cp bcc.sh /usr/local/bin/bcc
 if [ ! -d /usr/local/brainfunc/ ]; then
-    sudo mkdir /usr/local/brainfunc/
+    sudo mkdir -p /usr/local/brainfunc/
 fi
 sudo cp bc2c.c /usr/local/brainfunc/bc2c.c
