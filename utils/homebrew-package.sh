2b3c899537d36c2ca07891182a1812ad63f5073a
shuzaei <shuzaei@gmail.com>
2021-06-29 20:49:21 +0900

    Add template

--- utils/homebrew-package.sh
+++ utils/homebrew-package.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ $# = 1 ]; then
+    version="$1"
+else
+    printf "Usage: %s <version>\n" "$0"
+    exit 1
+fi
+
 root="$(dirname "$0")/.."
 
 if [ ! -d "$root/out/" ]; then
@@ -7,4 +14,11 @@ if [ ! -d "$root/out/" ]; then
 fi
 
 sudo tar -zcvf "$root/out/homebrew-package.tar.gz" bcc
-sha256sum "$root/out/homebrew-package.tar.gz" | awk '{print $1}' > "$root/out/homebrew-package.tar.gz.sha256sum"
+
+sum="$(sha256sum "$root/out/homebrew-package.tar.gz" | awk '{print $1}')"
+
+printf "\nSHA256 hashsum of %s:\n$sum\n" "homebrew-package.tar.gz"
+printf "$sum" > "$root/out/homebrew-package.tar.gz.sha256sum"
+
+sed -e "s/__VERSION__/$version/g" -e "s/__SHA256__/$sum/g" \
+    "$root/utils/homebrew-template.rb" > "$root/out/brainfunc.rb"
---
+++ utils/homebrew-template.rb
@@ -0,0 +1,40 @@
+#
+#   Copyright 2021 shuzaei
+#
+#   Contact: shuzaei@gmail.com
+#   URL: https://github.com/shuzaei/homebrew-brainfunc/
+#
+#
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Brainfunc < Formula
+  desc "A programming language like brainf*ck."
+  homepage "https://www.github.com/shuzaei/brainfunc"
+  url "https://github.com/shuzaei/brainfunc/releases/download/__VERSION__/homebrew-package.tar.gz"
+  version "__VERSION__"
+  sha256 "__SHA256__"
+  license "GNU GPL v3.0"
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    bin.install "bcc.sh" => "bcc"
+    system "cc", "-o", "bc2c", "bc2c.c"
+    bin.install "bc2c"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test brainfunc`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
