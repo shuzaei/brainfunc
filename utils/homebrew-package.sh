0af0438edf52ce10361a5907810bbf85ce73bdd7
Shuz* <42332130+shuzaei@users.noreply.github.com>
2021-06-29 19:52:16 +0900

    Update homebrew-package.sh

--- utils/homebrew-package.sh
+++ utils/homebrew-package.sh
@@ -7,4 +7,4 @@ if [ ! -d "$root/out/" ]; then
 fi
 
 sudo tar -zcvf "$root/out/homebrew-package.tar.gz" bcc
-sha256sum "$root/out/homebrew-package.tar.gz"
+sha256sum "$root/out/homebrew-package.tar.gz" | awk '{print $1}' > "$root/out/homebrew-package.tar.gz.sha256sum"
