405f6c6097aa9c5079908ac93e496a936d476689
shuzaei <shuzaei@gmail.com>
2021-07-06 02:47:13 +0900

    Fix typo

---
+++ docs/all.sh
@@ -0,0 +1,3 @@
+cd "$(dirname "$0")"
+marp --html --allow-local-files --title 'Brainfunc' brainfunc.md -o ./brainfunc.html
+marp --html --allow-local-files --title 'Brainfunc' brainfunc-dark.md -o ./brainfunc-dark.html
